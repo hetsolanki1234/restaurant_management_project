14f73ba4d2fe8396139c8af4be6b2cb8110b46b0
hetsolanki1234 <solankihet2005@gmail.com>
2025-09-04 23:02:28 +0530

    41604

--- restaurant.py/models.py
+++ restaurant.py/models.py
@@ -7,3 +7,11 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.email})"
+
+class MenuItem(models.Model):
+    name = models.CharField(max_lenght=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - $(self.price)"
